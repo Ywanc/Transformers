86aef3702c920e4dc5589c09a39151086945657d
KLY <52617170+Ywanc@users.noreply.github.com>
2024-09-26 22:34:36 +0800

    1

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 '''
 My attempt at replicating the transformer architecture from the "Attention Is All You Need"
-with help from the PyTorch Documentation & ChatGPT.
-Minimal reference to existing youtube videos. 
+with reference from 
+https://github.com/hkproj/pytorch-transformer, PyTorch Documentation, ChatGPT
 '''
 import torch
 from torch import nn
@@ -27,14 +27,14 @@ class InputEmbeddings(nn.Module):
         
 class PositionalEncodings(nn.Module):
     
-    def __init__(self, seq, d_model): #takes in seq & d_model
+    def __init__(self, seq_len, d_model, dropout): #takes in seq_len & d_model
         super().__init__()
-        self.seq = seq
+        self.seq_len = seq_len
         self.d_model = d_model
-        self.dropout = nn.Dropout()
+        self.dropout = nn.Dropout(dropout) #randomly zeroes elements of the tensor. to prevent overfitting
 
-        pe_matrix = torch.zeros(seq, d_model) # (seq, d_model)
-        pos = torch.arange(0,seq).unsqueeze(1) # (seq, 1)                           
+        pe_matrix = torch.zeros(seq_len, d_model) # (seq_len, d_model)
+        pos = torch.arange(0,seq_len).unsqueeze(1) # (seq_len, 1)                           
 
         #create tensor of 2i from i=0 to d_model, then /d_model & *ln10000 
         div = torch.exp(torch.arange(0, d_model, 2).float()/d_model * -math.log(10000)) # (d_model//2)
@@ -43,12 +43,55 @@ class PositionalEncodings(nn.Module):
         pe_matrix[:, 1::2] = torch.cos(pos * div) #select all row, col frm 1 with step 2
 
         #add batch dimension
-        pe_matrix = pe_matrix.unsqueeze(0) #(1, seq, d_model). 1 is batch_size
+        pe_matrix = pe_matrix.unsqueeze(0) #(1, seq_len, d_model). 1 is batch_size
 
         self.register_buffer("pe_matrix", pe_matrix) #tensor not parameter, but saved in state
     
     def forward(self, x):
-        return pe_matrix + x
+        return self.dropout(x + self.pe_matrix[:, :x.shape[1], :]) #only need pe_matrix until length of x
+
+class MultiheadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model, h, dropout):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.d_k = d_model//h
+
+        # the Query, Key, Value Matrices
+        self.W_q = nn.Linear(d_model, d_model, bias=False)
+        self.W_k = nn.Linear(d_model, d_model, bias=False)
+        self.W_v = nn.Linear(d_model, d_model, bias=False)
+        self.W_o = nn.Linear(d_model, d_model, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod #independent of instance
+    def attention(self, Q, K, V, mask, dropout): #takes in all heads of Q,K,V. (batch, h, seq, d_k), h head of (seq,d_k) matrices
+        d_k = Q.shape[-1]
+        attention_scores = (Q @ K.transpose(-2,-1))/math.sqrt(d_k) #transpose 1st last & 2nd last dimension. -> (batch, h, d_k, seq)
+
+        if mask is not None:
+            # fill -inf in positions where mask==0
+            attention_scores.masked_fill_(mask == 0, float('-inf'))
+
+        attention_weights = attention_scores.softmax(dim=-1) #apply softmax across "seq" dimension, -1
+        
+        if dropout: 
+            attention_weights = dropout(attention_weights) 
+
+        return attention_scores @ V
+
+    def forward(self, x, mask)
+
+        
+
+        
+        
+
+        
+
+        
+    
 
 
 
