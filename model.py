f2b671efe581a2229a1cf34460800709d83e0414
KLY <52617170+Ywanc@users.noreply.github.com>
2024-09-26 11:04:26 +0800

    1

--- model.py
+++ model.py
@@ -31,12 +31,29 @@ class PositionalEncodings(nn.Module):
         super().__init__()
         self.seq = seq
         self.d_model = d_model
-        self.pe_matrix = torch.randn()
-        
-        pe_matrix = torch.zeros(seq, d_model)
-        pos = torch.arange()
+        self.dropout = nn.Dropout()
+
+        pe_matrix = torch.zeros(seq, d_model) # (seq, d_model)
+        pos = torch.arange(0,seq).unsqueeze(1) # (seq, 1)                           
+
+        #create tensor of 2i from i=0 to d_model, then /d_model & *ln10000 
+        div = torch.exp(torch.arange(0, d_model, 2).float()/d_model * -math.log(10000)) # (d_model//2)
+
+        pe_matrix[:, 0::2] = torch.sin(pos * div) #select all row, col frm 0 with step 2
+        pe_matrix[:, 1::2] = torch.cos(pos * div) #select all row, col frm 1 with step 2
+
+        #add batch dimension
+        pe_matrix = pe_matrix.unsqueeze(0) #(1, seq, d_model). 1 is batch_size
+
+        self.register_buffer("pe_matrix", pe_matrix) #tensor not parameter, but saved in state
     
+    def forward(self, x):
+        return pe_matrix + x
+
+
 
         
+
+
         
         
